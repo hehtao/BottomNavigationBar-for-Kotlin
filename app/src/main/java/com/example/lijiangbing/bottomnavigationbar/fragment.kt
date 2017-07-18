e3cd788af183c56a79fe231ec394c0c3e7a17b85
hehtao <hehtao@163.com>
2017-07-18 09:21:47 +0800

    .

--- app/src/main/java/com/example/lijiangbing/bottomnavigationbar/fragment.kt
+++ app/src/main/java/com/example/lijiangbing/bottomnavigationbar/fragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.view.LayoutInflater
 import android.view.View
 import kotlinx.android.synthetic.main.fragment.*
+import kotlinx.android.synthetic.main.fragment.view.*
 
 
 class fragment(val title: String) : Fragment(){
@@ -21,10 +22,12 @@ class fragment(val title: String) : Fragment(){
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         super.onCreateView(inflater, container, savedInstanceState);
 
-        val mview = inflater?.inflate(R.layout.fragment,container,false) as View;
-        val tv = mview.findViewById(R.id.tv_titleView) as TextView
-        tv.text = title;
-        Log.e("textView","${tv.text}");
+        val mview = inflater?.inflate(R.layout.fragment,container,false)!!;
+        mview.tv_titleView.text = title;
+//        val tv = mview.findViewById(R.id.tv_titleView) as TextView
+//        tv.text = title;
+//        tv_titleView.text = title;  此处不可直接使用 Kotlin-extention 调用  tv_titleView,
+//                                    在本方法返回mView之后,mView(fragment)才会被渲染绘制,对应的 tv_titleView 为 null
         return mview;
     }
 
